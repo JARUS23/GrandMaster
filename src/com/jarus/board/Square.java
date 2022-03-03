4ec8ead64bacb1e9400683a5f3fa6c54af72a7d0
suraj <surajgandhi23@gmail.com>
2022-03-03 18:50:13 +0530

    Added reference to Board state and added draw call

--- src/com/jarus/board/Square.java
+++ src/com/jarus/board/Square.java
@@ -1,8 +1,29 @@
 package com.jarus.board;
 
+import com.jarus.utils.Images;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class Square extends JButton {
 
+    private BoardState boardState;
+    private int i, j;
+
+    public Square(BoardState boardState, int corX, int corY)
+    {
+        this.boardState = boardState;
+        i = corX;
+        j = corY;
+    }
+
+    @Override
+    public void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+
+        if (boardState.getBoardState()[i][j] != null)
+            boardState.getBoardState()[i][j].draw(g);
+    }
 
 }
