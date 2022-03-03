19a7d89f5334089f144fceb32de4f1e575ac8cce
suraj <surajgandhi23@gmail.com>
2022-03-03 18:55:09 +0530

    Images loaded and object of Board state created

--- src/com/jarus/windows/GameWindow.java
+++ src/com/jarus/windows/GameWindow.java
@@ -1,26 +1,35 @@
 package com.jarus.windows;
 
+import com.jarus.board.BoardState;
 import com.jarus.board.ChessBoard;
+import com.jarus.utils.Images;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class GameWindow extends JFrame {
 
+    private BoardState boardState;
     private ChessBoard chessBoard;
 
     public GameWindow()
     {
         super("Grand Master");
 
+        // Loading all Images
+        Images.init();
+
+        // Creating the state of our Chess Board
+        boardState = new BoardState();
+
+        // Creating ChessBoard JPanel and adding it to the JFrame
+        chessBoard = new ChessBoard(boardState, new Dimension(480, 480));  // TODO Try it with 482 value later
+        add(chessBoard, BorderLayout.CENTER);
+
         // Getting the Screen Size
         Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
         double width = size.getWidth();
         double height = size.getHeight();
-
-        // Creating ChessBoard JPanel and adding it to the JFrame
-        chessBoard = new ChessBoard(new Dimension(480, 480));  // TODO Try it with 482 value later
-        add(chessBoard, BorderLayout.CENTER);
 
         // Setting Window Properties
         setLocation((int) width / 2 - 300, (int) height / 2 - 200);   // TODO Set the proper location of the window
