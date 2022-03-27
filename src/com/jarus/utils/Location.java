22678f0dd7c95eb8014e437d23e8d59a4765c0f3
suraj <surajgandhi23@gmail.com>
2022-03-27 23:13:27 +0530

    Added Location class

---
+++ src/com/jarus/utils/Location.java
@@ -0,0 +1,25 @@
+package com.jarus.utils;
+
+public class Location {
+
+    private int x;
+    private int y;
+
+    public Location(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    // GETTER
+    public int getX()
+    {
+        return x;
+    }
+
+    // GETTER
+    public int getY()
+    {
+        return y;
+    }
+}
